581f1a2f1f328ba4511e998afd3e832a79dc9354
Lilia2705 <l.dovgun@gmail.com>
2019-10-30 14:53:29 +0100

    First project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,58 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'First';
+  inputedValue:string;
+  buttonValue: string;
+
+  isLoginClicked = false;
+  isRegisterClicked = false;
+
+  houses = [
+    {
+      city: 'Lviv',
+      street: 'Shevchenka',
+      price: 450000
+
+    },
+    {
+      city: 'Kyiv',
+      street: 'Kotova',
+      price: 28000
+
+    },
+    {
+      city: 'Odessa',
+      street: 'Derybasivska',
+      price: 100000
+
+    }
+  ]
+
+
+
+
+
+  setName(ev) {
+    this.inputedValue = ev.target.value;
+  }
+  buttonClick() {
+  this.buttonValue = this.inputedValue
+  }
+
+  showLoginForm (){
+  this.isLoginClicked = !this.isLoginClicked;
+  this.isRegisterClicked = false;
 }
+  showRegisterForm (){
+    this.isRegisterClicked = !this.isRegisterClicked;
+    this.isLoginClicked = false;
+  }
+
+}
+
+
+
